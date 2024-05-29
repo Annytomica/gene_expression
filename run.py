d4817df4718be4e6c63cbca095ea5678172a0371
Annytomica <annydevoyalt@gmail.com>
2024-05-29 09:17:38 +0000

    Add methods to convert expression value to float and round to 2 decimal places for output

--- run.py
+++ run.py
@@ -15,16 +15,21 @@ SHEET = GSPREAD_CLIENT.open('FUSdelta14_gene_expression_database')
 
 NAMES = SHEET.worksheet("expression").col_values(1)
 ENSEMBL = SHEET.worksheet("expression").col_values(2)
+EXPRESSION = SHEET.worksheet("expression").col_values(5)
 DATA = SHEET.worksheet("expression").get_all_values()
 HEADINGS = DATA[0]
 
 user_gene = input('Enter gene name here: ').capitalize()
-user_ensembl = input('Enter Ensembl ID here: ').upper()
+#user_ensembl = input('Enter Ensembl ID here: ').upper()
 
 gene_index = NAMES.index(user_gene)
 gene_data = DATA[gene_index]
+expression_data = float(EXPRESSION[gene_index])
 print(gene_data)
+print(round(expression_data, 2))
 
+'''
 ensembl_index = ENSEMBL.index(user_ensembl)
 ensembl_data = DATA[ensembl_index]
 print(ensembl_data)
+'''
